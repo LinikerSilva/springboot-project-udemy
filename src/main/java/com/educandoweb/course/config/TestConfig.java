23b9ae4a8838e7f9ad07589677a58789a7b8d323
Liniker Silva <linikergabrieldasilva@gmail.com>
2022-03-01 16:51:44 -0300

    feat: implementing product and manyToMany association with joinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.educandoweb.course.config;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,11 +30,30 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Category category1 = new Category(null, "Electronics");
         Category category2 = new Category(null, "Books");
         Category category3 = new Category(null, "Computers");
+
+        Product product1 = new Product(null, "The Lord of the Rings", "Lorem impsum dolor sit amet, consectetur.", 90.5, "");
+        Product product2 = new Product(null, "Smart TV", "Lorem impsum dolor sit amet, consectetur.", 2190.0, "");
+        Product product3 = new Product(null, "Macbook Pro", "Lorem impsum dolor sit amet, consectetur.", 1250.0, "");
+        Product product4 = new Product(null, "PC Gamer", "Lorem impsum dolor sit amet, consectetur.", 1200.0, "");
+        Product product5 = new Product(null, "Rails for Dummies", "Lorem impsum dolor sit amet, consectetur.", 100.99, "");
+
+        product1.getCategories().add(category2);
+        product2.getCategories().add(category1);
+        product2.getCategories().add(category3);
+        product3.getCategories().add(category3);
+        product4.getCategories().add(category3);
+        product5.getCategories().add(category2);
+
+        categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+        productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
 
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777", "123456");
@@ -44,5 +65,6 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
         categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+        productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
     }
 }
